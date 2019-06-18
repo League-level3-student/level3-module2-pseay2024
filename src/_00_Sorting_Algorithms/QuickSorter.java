b24b0dce067f040e1fc27c39b6dbd22365a6f667
pseay2024 <46167219+pseay2024@users.noreply.github.com>
2019-06-17 18:24:55 -0700

    DNA Sequences

--- src/_00_Sorting_Algorithms/QuickSorter.java
+++ src/_00_Sorting_Algorithms/QuickSorter.java
@@ -13,33 +13,58 @@ public class QuickSorter extends Sorter {
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 		//10. call the quickSort method with 0 and the length of the array minus one
+		quickSort(array,0,array.length - 1, display);
 	}
 	
-	private void quickSort(int[] array, int low, int high, SortingVisualizer display) {
+	private void quickSort(int[] a, int low, int high, SortingVisualizer display) {
 		//2. create two integer variables called i and j and set them equal
 		//   to low and high respectively.
-       
+        int i = low;
+        int j = high;
         //3. create an integer called pivot and set it equal to the element
         //   in the array that is the halfway point between low and high
-
+        int pivot = a[(high + low)/2];
         //4. make a while loop that goes while i is less than or equal to j
-        
+        while (i <= j)
+        {
         	//5. make a while loop that increments i while 
         	//   the array element at i is less than pivot
-            
+        	while (a[i] < pivot)
+        	{
+        		i++;
+        	}
             //6. make a while loop that decrements j while 
         	//   the array element at j is greater than pivot
-            
+            while (a[j] > pivot)
+            {
+            	j--;
+            }
             //7. if i is less than or equal to j, 
             //   swap the array elements at i and j.
             //   In the same if-statement, increase i by 1 
             //   and decrease j by 1.
-        
+            if ( i <= j)
+            {
+            	int temp = a[i];
+            	a[i] = a[j];
+            	a[j] = temp;
+            	display.updateDisplay();
+            	i+=1;
+            	j-=1;
+            }
+        }
         //8. if low is less than j, call the quickSort method using
         //   low for the low and j for the high
-        
+        if (low < j)
+        {
+        	quickSort(a,low,j,display);
+        }
         //9. if i is less than high, call the quickSort method using
         //   i for the low and high for the high.
+        if (i < high)
+        {
+        	quickSort(a,i,high,display);
+        }
 	}
 
 }
--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,9 +1,67 @@
 package _02_More_Algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		return eggs.indexOf("cracked");
+	}
+	public static int countPearls(List<Boolean> bs)
+	{
+		int r = 0;
+		for (boolean b : bs)
+		{
+			if (b==true) r++;
+		}
+		return r;
+	}
+	public static double findTallest(List<Double> peeps)
+	{
+		double t = 0;
+		for (double d : peeps)
+		{
+			if (t < d) t = d;
+		}
+		return t;
+	}
+	public static String findLongestWord(List<String> ws)
+	{
+		int t = ws.get(0).length();
+		String r = ws.get(0);
+		for (String s : ws)
+		{
+			if (s.length() > r.length()) r = s; t = r.length();
+		}
+		return r;
+	}
+	//containsSOS
+	public static boolean containsSOS(List<String> s)
+	{
+		return s.indexOf(" ... --- ... ")>=0;
+	}
+	public static List<Double> sortScores(Double[] results)
+	{
+		for (int i = 0; i < results.length; i++)
+		{
+			for (int j = 1; j < results.length - i; j++)
+			{
+				if (results[j - 1] > results[j])
+				{
+					double temp = results[j-1];
+					results[j-1] = results[j];
+					results[j] = temp;
+				}
+			}
+		}
+		List<Double> r = new ArrayList<Double>();
+		for (double d : results)
+		{
+			r.add(d);
+		}
+		return r;
 	}
 }
